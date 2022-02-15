ab39ebb37047df0cfc546503aba1639a1e36d6be
jiyoung <shyunji023@gmail.com>
2022-02-15 17:48:14 +0900

    한글 테스트

--- ProJect01/src/Pack01/World.java
+++ ProJect01/src/Pack01/World.java
@@ -3,7 +3,6 @@ package Pack01;
 import java.util.Scanner;
 
 public class World {
-
 	public static void main(String[] args) {
 		System.out.println("여기는 월드");
 		Scanner sc = new Scanner(System.in);
