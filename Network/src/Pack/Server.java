8c7edb9edace432b44ebb1752995e018ce26ed25
jiyoung <shyunji023@gmail.com>
2022-02-15 15:30:09 +0900

    쓰레드 처리

--- Network/src/Pack/Server.java
+++ Network/src/Pack/Server.java
@@ -21,23 +21,27 @@ public class Server extends Application {
 		
 		Button btn1 = new Button("서버오픈");
 		btn1.setOnAction(new EventHandler<ActionEvent>() {
-			
 			@Override
 			public void handle(ActionEvent arg0) {
-				try {
-					ServerSocket ss = new ServerSocket();
-					System.out.println("메인서버 소켓 생성");
-					ss.bind(new InetSocketAddress("localhost",5001));
-					System.out.println("바인딩 완료");
-					// 블로킹 함수이다. 누군가가 접속을 해주면 풀림 
-					// 버튼에서 빠져나오지 못하니까 다음버튼을 누르지 못함
-					// 무한루프에 빠짐
-					ss.accept();
-					System.out.println("누군가 접속을 시도했음");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				
+				new Thread() {
+					@Override
+					public void run() {
+						try {
+							ServerSocket ss = new ServerSocket();
+							System.out.println("메인서버 소켓 생성");
+							ss.bind(new InetSocketAddress("localhost",5001));
+							System.out.println("바인딩 완료");
+							// 블로킹 함수이다. 누군가가 접속을 해주면 풀림 
+							// 버튼에서 빠져나오지 못하니까 다음버튼을 누르지 못함
+							// 무한루프에 빠짐
+							// 쓰레드로 해결
+							ss.accept();
+							System.out.println("누군가 접속을 시도했음");
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}.start();
 			}
 		});
 		root.getChildren().add(btn1);
