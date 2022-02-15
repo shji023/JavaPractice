5bf51b88a61f392f7e274c232624cb2c8f399a91
jiyoung <shyunji023@gmail.com>
2022-02-15 17:23:38 +0900

    이중 console창

--- ProJect01/src/Pack01/Hello3.java
+++ ProJect01/src/Pack01/Hello3.java
@@ -65,7 +65,7 @@ public class Hello3 extends Application {
 */
 
 // ex89)
-public class Hello3 {
+public class Hello3 { 
 	public static void main(String[] args) {
 		System.out.println("여기는 헬로우");
 		Scanner sc = new Scanner(System.in);
---
+++ ProJect01/src/Pack01/World.java
@@ -0,0 +1,14 @@
+package Pack01;
+
+import java.util.Scanner;
+
+public class World {
+
+	public static void main(String[] args) {
+		System.out.println("여기는 월드");
+		Scanner sc = new Scanner(System.in);
+		sc.nextInt();		
+		System.out.println("헬로우 종료");
+	}
+
+}
