7064f3e5be5ca3c2c5324028fc10266e6994d364
jiyoung <shyunji023@gmail.com>
2022-02-16 17:44:13 +0900

    최종

---
+++ NTest/src/pack01/ClientTest.java
@@ -0,0 +1,171 @@
+package pack01;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ClientTest extends Application {
+	Socket cs;
+	TextArea textArea;
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		VBox root = new VBox();
+		HBox root2 = new HBox();
+		HBox root3 = new HBox();
+		root.setPrefSize(400, 300);
+		root.setSpacing(5);
+
+		textArea = new TextArea();
+		textArea.setEditable(false);
+		
+		TextField textInput = new TextField();
+		TextField userName = new TextField();
+		userName.setPrefWidth(100);
+		userName.setPromptText("닉네임을 입력하세요");
+
+		
+		// 서버 접속
+		Button startButton = new Button("접속버튼");
+		startButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					userName.setDisable(true);
+//					cs = new Socket();
+//					cs.connect(new InetSocketAddress("localhost",5001));
+//					ConnectThread ct = new ConnectThread();
+//					ct.start();	
+					new Thread() {
+						public void run() {
+							try {
+								cs = new Socket();
+								cs.connect(new InetSocketAddress("192.168.0.79",5001));
+								receive();
+
+							} catch (Exception e) {
+								// TODO: handle exception
+							}
+						};
+					}.start();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+		
+		// 입력 테스트 전송
+		textInput.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				// 전송할 수 있는 타입은 딱한가지 byte타입
+				try {
+					 
+					DataOutputStream os  = new DataOutputStream( cs.getOutputStream());
+					String s = userName.getText() + " : " + textInput.getText();
+					
+					os.writeUTF(s);
+					textInput.setText("");
+					System.out.println("데이터 보냄");
+				} catch (Exception e) {e.printStackTrace();}
+				
+			}
+		});
+		
+
+		// 접속 종료
+		Button stopButton = new Button("접속종료");
+		stopButton.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					DataOutputStream os  = new DataOutputStream( cs.getOutputStream());
+					String s = userName.getText() + "님이 접속 종료했습니다. ";
+					
+					os.writeUTF(s);
+					cs.close();
+					} catch (Exception e) {}
+			}
+		});
+		// 보내기 버튼
+		Button sendButton = new Button("전송");
+	      sendButton.setOnAction(new EventHandler<ActionEvent>() {
+	         @Override
+	         public void handle(ActionEvent arg0) {
+	            try {
+	            	DataOutputStream os  = new DataOutputStream( cs.getOutputStream());
+					 String s = textInput.getText();
+	              
+	               os.writeUTF(s);
+	               textInput.setText("");
+	               System.out.println("데이터 보냄");
+	            } catch (Exception e) {   e.printStackTrace(); }
+	         }
+	      });
+		
+		root2.getChildren().addAll(startButton,stopButton);
+		root3.getChildren().addAll(textInput,sendButton);
+		root.getChildren().addAll(userName, root2, textArea, root3);
+
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Client");
+		stage.show();
+
+	}
+
+	// 클라이언트 프로그램 종료
+	public void stopClient() {
+		try {
+			if(cs != null && !cs.isClosed()) {
+				cs.close();
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	// 서버로부터 메시지 받음
+	public void receive() {
+		while(true) {
+			try {
+				DataInputStream in=new DataInputStream(cs.getInputStream());
+				String message = in.readUTF();
+				
+				System.out.println(message);
+				Platform.runLater(()->{
+					textArea.appendText(message);
+					textArea.appendText("\n");
+				});
+			}catch(Exception e) {
+				stopClient();
+				break;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
---
+++ NTest/src/pack01/ServerTest.java
@@ -0,0 +1,168 @@
+package pack01;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+
+
+class ConnectThread extends Thread {
+   Socket socket;
+   Scanner sc;
+   ServerSocket serverSocket;
+   LinkedList<Socket> socketList;
+   Map<Socket, Integer> socketMap;
+   ServerTest SPJ;
+   int id;
+   ConnectThread(ServerTest SPJ){
+      this.SPJ = SPJ;
+   }
+   @Override
+   public void run() {
+      try {
+         serverSocket = new ServerSocket(5001); // listen 상태 소켓을 생성
+         socketList = new LinkedList<>();
+         socketMap = new HashMap<>();
+         id = 0; //아이디
+         System.out.println("서버 실행");
+         while (true) {
+            // 클라이언트가 다중으로 접속하기 위한 무한반복문
+            
+            socket = serverSocket.accept();
+            // connect 요청이 들어오면 연결 (올때까지 block)
+            
+            Platform.runLater(()->{
+                  ++id;
+                     SPJ.lab.setText("인원수: "+id+"\n");
+                  });
+            System.out.println("클라이언트 접속");
+            System.out.println(socket);
+            socketMap.put(socket,id);
+            socketList.add(socket);
+            
+            // 클라이언트마다 스레드를 생성
+            // Thread가 없을경우 readUTF() 메서드에서 block이 발생하기 때문에 독립적으로 분할한다.  
+            new Thread(()->{
+               try {
+                  DataInputStream in = new DataInputStream(socket.getInputStream());
+                  revMsg(in,socket);
+               } catch (IOException e) {
+                  
+               }
+            }).start();
+         }
+      } catch (IOException e) {
+      }
+
+
+
+   }
+
+   public void revMsg(DataInputStream in,Socket client){
+      Boolean nameCheck = true;
+      String name = "";
+      try {
+         
+         while (true) {
+            String msg = in.readUTF();
+            if(nameCheck) {
+               name = msg;
+               nameCheck = false;
+            }
+            else if(!msg.equals("")) {
+               sendMsg(client,name+": "+msg);
+               System.out.println(msg);
+            }
+            else {
+               System.out.println("빈 내용을 입력하지 말아주세요!");
+            }
+         }
+      } catch (IOException e) {
+         if(nameCheck) {
+         System.out.println(socket+"님 접속 종료");
+         
+         Platform.runLater(()->{
+            --id;
+            SPJ.lab.setText("인원수: "+id+"\n");
+            });
+         }
+         else {
+         System.out.println(name +"님 접속 종료");
+         
+         Platform.runLater(()->{
+            --id;
+            SPJ.lab.setText("인원수: "+id+"\n");
+            });
+         }
+      }
+   }
+   public void sendMsg(Socket client,String msg){
+      Integer id = socketMap.get(client);
+      socketList.stream().forEach(so->{
+         try {
+
+            DataOutputStream ou = new DataOutputStream(so.getOutputStream());
+            ou.writeUTF(msg);
+
+         } catch (IOException e) {
+         }
+      });
+   }
+
+
+
+
+}
+
+public class ServerTest extends Application { // 클라이언트를 종료하고 서버를 종료해야한다.
+   Label lab;
+   Button btn1;
+   public void start(Stage stage) throws Exception {
+      VBox root = new VBox();
+      root.setPrefSize(400, 300);
+      root.setSpacing(5);
+      lab = new Label("인원수: 0");
+      btn1 = new Button("서버 오픈");
+      btn1.setOnAction(new EventHandler<ActionEvent>() {
+
+         @Override
+         public void handle(ActionEvent arg0) {
+            // TODO Auto-generated method stub
+            ConnectThread ct = new ConnectThread(ServerTest.this);
+            ct.start();
+         }
+      });
+      
+      root.getChildren().addAll(btn1, lab);
+      Scene scene = new Scene(root);
+      stage.setScene(scene);
+      stage.setTitle("Server");
+      stage.show();
+   }
+
+   public static void main(String[] args) {
+      launch();      
+
+      System.out.println("서버 종료");
+   }
+}
