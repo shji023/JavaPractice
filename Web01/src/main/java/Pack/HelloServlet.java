61404574b1548b21b544291dfa93db72e14f7a16
jiyoung <shyunji023@gmail.com>
2022-02-24 16:38:55 +0900

    feat: monkey 출력

--- Web01/src/main/java/Pack/HelloServlet.java
+++ Web01/src/main/java/Pack/HelloServlet.java
@@ -1,6 +1,8 @@
 package Pack;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,6 +23,15 @@ public class HelloServlet extends HttpServlet {
 			HttpServletRequest request, 
 			HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("doGet Call"); // 웹 브라우저의 요청이 있으면 자바 코드를 호출한다!
+		// out을 이용해서 html을 만든다
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+			out.println("<head>");
+			out.println("</head>");
+			out.println("<body>");
+				out.println("<h1>Monkey</h1>");
+			out.println("</body>");
+		out.println("</html>");
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
