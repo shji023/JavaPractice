2c04a4fd6e249ca06bca058995479451dce428b5
jiyoung <shyunji023@gmail.com>
2022-03-07 15:40:07 +0900

    spring helloworld

---
+++ SimpleSpring01/src/main/java/Pack/Hello.java
@@ -0,0 +1,20 @@
+package Pack;
+
+import org.springframework.context.annotation.Configuration;
+
+class Apple {
+	
+}
+
+@Configuration
+class AppConfig {
+	
+}
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
