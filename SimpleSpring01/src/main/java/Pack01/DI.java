294af775fbdc2a1dc9381121e57555ba42838ed3
jiyoung <shyunji023@gmail.com>
2022-03-08 10:14:46 +0900

    직접 di

--- SimpleSpring01/src/main/java/Pack01/DI.java
+++ SimpleSpring01/src/main/java/Pack01/DI.java
@@ -5,7 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 class Apple {
-
+	void f1() {
+		System.out.println("test");
+	}
+}
+class Orange {
+	Apple apple;
+	Orange(Apple apple){
+		this.apple = apple;
+	}
+	void f2() {
+		apple.f1();
+	}
 }
 @Configuration
 class AppConfig {
@@ -23,6 +34,10 @@ public class DI {
 		AnnotationConfigApplicationContext ctx = 
 				new AnnotationConfigApplicationContext(AppConfig.class);
 		System.out.println(ctx.getBeanDefinitionCount()); //default 5개 + 방금 만든 한개
+		
+		Orange orange = new Orange(new Apple());
+		orange.f2(); // 사용자가 직접적으로 주입을 시켜서 사용함 
+		
 		ctx.close();
 		
 		
