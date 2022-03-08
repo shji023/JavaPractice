c984dc7a82b1fe936c7694a614079b28f8061bd1
jiyoung <shyunji023@gmail.com>
2022-03-08 10:36:05 +0900

    세터주입

--- SimpleSpring01/src/main/java/Pack01/DI.java
+++ SimpleSpring01/src/main/java/Pack01/DI.java
@@ -11,12 +11,19 @@ class Apple {
 	}
 }
 class Orange {
-	@Autowired // 밑에 주석잡은 생성자 코드를 만든다.
+	@Autowired // 밑에 주석잡은 생성자 코드를 만든다. 필드 주입
+	// 1. 사용하지 않는것을 권장 (스프링 측)
+	// 2. 실전에서 사용을 많이 함 
 	Apple apple;
 	// 생성자 주입
 //	Orange(Apple apple){
 //		this.apple = apple;
 //	}
+	// 생성자 뿐 만 아니라 setter함수 사용하여 주입가능
+	// 세터 주입(수정자 주입)
+	void setApple(Apple apple) {
+		this.apple = apple;
+	}
 	void f2() {
 		apple.f1();
 	}
@@ -30,7 +37,10 @@ class AppConfig {
 	@Bean()
 	Orange orange() {
 		//return new Orange(apple()); // 원래의 apple을 그대로 쓰고있다. singleton
-		return new Orange(); // Autowired
+		Orange o = new Orange();
+		o.setApple(apple());
+		return o;
+		//return new Orange(); // Autowired
 	}
 
 }
@@ -45,7 +55,9 @@ public class DI {
 		
 		// Orange orange = new Orange(new Apple());
 		// orange.f2(); // 사용자가 직접적으로 주입을 시켜서 사용함 
-		
+		Orange o = new Orange();
+		o.setApple(new Apple());
+		o.f2(); // 세터주입 적용 방법, 스프링 방식x
 		Orange orange = ctx.getBean("orange", Orange.class);
 		orange.f2(); // 스프링활용 주입 코드
 		ctx.close();
