922b68af24269a009b7cf6768c595a2588145520
jiyoung <shyunji023@gmail.com>
2022-04-19 12:27:14 +0900

    applicationContext

--- MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
+++ MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
@@ -1,13 +1,40 @@
 package Pack01;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
 
-@SpringBootTest
-class Msa01ApplicationTests {
-
-	@Test
-	void contextLoads() {
+@Service("tiger")
+class Tiger {
+	Tiger(){
+		System.out.println("생성자 콜");
 	}
-
+	void f1() {
+		System.out.println("f1 콜");
+	}
+}
+@SpringBootTest
+class Msa01ApplicationTests implements ApplicationContextAware{
+	ApplicationContext applicationContext;
+	public void setApplicationContext(
+			ApplicationContext applicationContext)
+					throws BeansException{
+		System.out.println("setApplicationContext");
+		this.applicationContext = applicationContext;
+	}
+	
+	@Test
+	void test01() {
+		System.out.println(100);
+		try {
+		Tiger t = applicationContext.getBean("tiger",Tiger.class);
+		System.out.println(t.hashCode());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
