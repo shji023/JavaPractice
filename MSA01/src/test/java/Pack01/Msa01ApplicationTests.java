99f0046ed3d2082d121012bb6f4617e630c59a6e
jiyoung <shyunji023@gmail.com>
2022-04-19 16:06:40 +0900

    곱셈 문제 내기 프로그램

--- MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
+++ MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
+
+// Multiplication
+// RandomGeneratorService
+// RandomGeneratorServiceImpl
+// MultiplicationService
+// MultiplicationServiceImpl
 class  Multiplication {
 	int factorA, factorB;
 	int result;
@@ -24,14 +31,54 @@ class  Multiplication {
 	public String toString() {
 		return "Multiplication [factorA=" + factorA + ", factorB=" + factorB + ", result=" + result + "]";
 	}
-	
-	
 }
+// ex) B
+interface RandomGeneratorService{
+	int generateRandomFactor();
+}
+@Service
+class RandomGeneratorServiceImpl implements RandomGeneratorService{
+	@Override
+	public int generateRandomFactor() {
+//		int a = 10; 최솟값
+//		int b = 20; 최댓값
+//		return new Random().nextInt(b-a+1)+10; 최솟값과 최댓값 사이에서 랜덤하게 추출
+		return new Random().nextInt(100);
+	}
+}
+
+// ex) C
+interface MultiplicationService{
+	Multiplication createRandomMultiplication();
+}
+@Service
+class MultiplicationServiceImpl implements MultiplicationService{
+	@Autowired
+	RandomGeneratorService randomGeneratorService;
+	
+//	@Autowired
+//	MultiplicationServiceImpl (RandomGeneratorService randomGeneratorService){
+//		this.randomGeneratorService = randomGeneratorService;
+//	}
+	@Override
+	public Multiplication createRandomMultiplication() {
+		int factorA = randomGeneratorService.generateRandomFactor();
+		int factorB = randomGeneratorService.generateRandomFactor();
+		return new Multiplication(factorA, factorB);
+	}
+}
+
 @SpringBootTest
 class Msa01ApplicationTests{
+//	@MockBean
+//	RandomGeneratorService randomGeneratorService;
+	
+	@Autowired
+	MultiplicationServiceImpl multiplicationServiceImpl;
+	
 	@Test
 	void test01() {
-		Multiplication m = new Multiplication(3, 4);
+		Multiplication m = multiplicationServiceImpl.createRandomMultiplication();
 		System.out.println(m.toString());
 	}
 }
