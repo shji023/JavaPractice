78156a4a4bcbca3bd36f40c9b67a92adc73d0e15
jiyoung <shyunji023@gmail.com>
2022-04-19 11:36:27 +0900

    MSA initial setting

---
+++ MSA01/src/main/java/Pack01/Msa01Application.java
@@ -0,0 +1,13 @@
+package Pack01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msa01Application.class, args);
+	}
+
+}
