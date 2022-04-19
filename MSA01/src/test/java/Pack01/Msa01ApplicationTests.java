2d391baf7f4dac349e118dbd4b01156a0ee18784
jiyoung <shyunji023@gmail.com>
2022-04-19 14:26:25 +0900

    주입주입

--- MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
+++ MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
@@ -1,5 +1,7 @@
 package Pack01;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,25 +10,49 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
-@Service
 class A {
-	void f1() {
-		System.out.println("call 1");
+	int a,b;
+	A(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+	int getResult() {
+		return a*b;
 	}
 }
-class B {
+interface B {
+	int getNum();
+}
+@Service
+class BImpl implements B{
+	@Override
+	public int getNum() {
+		return new Random().nextInt(10);
+	}
+}
+
+interface C{
+	A f1();
+}
+@Service
+class CImpl implements C{
 	@Autowired
-	A a;
-	void f2() {
-		System.out.println("call 2");
-		System.out.println(a);
+	B b;
+	
+	@Override
+	public A f1() {
+		return new A(b.getNum(), b.getNum());
 	}
 }
 @SpringBootTest
 class Msa01ApplicationTests{
+	@Autowired
+	C c;
+	
 	@Test
 	void test01() {
-		B b = new B();
-		b.f2();
+		A a = c.f1();
+		System.out.println(a.a+" "+a.b);
+		System.out.println(a.getResult());
 	}
 }
