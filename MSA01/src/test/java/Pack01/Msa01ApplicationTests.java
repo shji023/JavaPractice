4bc7ecf4060b0462e048752f526564aca879e7a8
jiyoung <shyunji023@gmail.com>
2022-04-20 09:32:47 +0900

    객체 지향 순서에 따라 코드 작성하기 - 초기 상태

--- MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
+++ MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
@@ -12,57 +12,11 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 import static org.mockito.BDDMockito.*;
 
-// 곱셈 문제지
-class Multiple {
-	int factorA, factorB;
-	int result;
-	Multiple(int factorA, int factorB){
-		this.factorA = 1;
-		this.factorB = 2;
-		this.result = this.factorA*this.factorB;
-	}
-	void show() {
-		System.out.println("factorA:"+factorA);
-		System.out.println("factorB:"+factorB);
-		System.out.println("result:"+result);
-	}
-}
-interface RandomGenService {
-	int getGenRandom();
-}
-// 난수 생성 서비스
-@Service
-class RandomGenServiceImpl implements RandomGenService{
-	public int getGenRandom() {
-		return new Random().nextInt(10);
-	}
-	
-}
-interface MultipleService{
-	Multiple getMultiple();
-}
-// 곱셈 생성 서비스
-@Service
-class MultipleServiceImpl implements MultipleService {
-	@Autowired
-	RandomGenService rnd;
-	public Multiple getMultiple() {
-		int a = rnd.getGenRandom();
-		int b = rnd.getGenRandom();
-		a = 3;
-		b = 4;
-		return new Multiple(a, b);
-	}
-}
 
 @SpringBootTest
 class Msa01ApplicationTests{
-	@Autowired
-	MultipleService multipleService;
 	@Test
 	void test01() {
-		Multiple multiple = multipleService.getMultiple();
-		multiple.show();
 	}
 }
 
