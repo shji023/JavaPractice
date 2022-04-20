9518d48460d035b7c19e2f9db68f968b733e736a
jiyoung <shyunji023@gmail.com>
2022-04-20 15:05:40 +0900

    get

--- MSA01/src/main/java/Pack01/Tiger.java
+++ MSA01/src/main/java/Pack01/Tiger.java
@@ -2,12 +2,23 @@ package Pack01;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//public class Tiger {
+//	@RequestMapping("/t1")
+//	String f1() {
+//		System.out.println("들어옴");
+//		return "TigerView";
+//	}
+//}
+
+
+@RestController
 public class Tiger {
 	@RequestMapping("/t1")
 	String f1() {
 		System.out.println("들어옴");
-		return null;
+		return "호랑이";
 	}
 }
--- MSA01/src/main/webapp/WEB-INF/views/index.jsp
+++ MSA01/src/main/webapp/WEB-INF/views/index.jsp
@@ -6,8 +6,21 @@
 <meta charset="EUC-KR">
 <title>Insert title here</title>
 </head>
+<script src="//code.jquery.com/jquery-3.3.1.js"></script>
+<script type="text/javascript">
+	function f1(){
+		console.log("클릭됨");
+		$.ajax({
+			url:"http://localhost:8081/t1",
+			type:"GET"
+		}).then(function(data){
+			console.log(data);
+		});
+	}
+</script>
 <body>
 	<h1>I want to go home.</h1>
 	<a href="t1">기본 컨트롤러 확인</a>
+	<button onClick="f1()">클릭 확인용</button>
 </body>
 </html>
