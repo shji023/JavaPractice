1056f6c799cd2233ea6973ad70c30fcb2eed1dc0
jiyoung <shyunji023@gmail.com>
2022-04-20 12:26:54 +0900

    User(alias=호랑이) 호랑이

--- MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
+++ MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
@@ -15,32 +15,70 @@ import lombok.*;
 
 import static org.mockito.BDDMockito.*;
 
-//class Tiger {
-//	final String name;
-//	final int age;
-//	Tiger(){
-//		this("",0);
-//	}
-//	Tiger(String name, int age){
-//		this.name = name;
-//		this.age = age;
-//	}
-//}
+class Multiple {
+	int factorA, factorB;
+	int result;
+	Multiple(int factorA, int factorB){
+		this.factorA = 1;
+		this.factorB = 2;
+		this.result = this.factorA*this.factorB;
+	}
+	void show() {
+		System.out.println("factorA:"+factorA);
+		System.out.println("factorB:"+factorB);
+		System.out.println("result:"+result);
+	}
+}
+interface RandomGenService{
+	int getGenRandom();
+}
+@Service
+class RandomGenServiceImpl implements RandomGenService{
+	public int getGenRandom() {
+		return new Random().nextInt(10);
+	}
+}
+class MultiplicationResultAttempt {
+	
+}
+interface MultipleService {
+	Multiple getMultiple(); 
+	boolean checkAttempt(MultiplicationResultAttempt restultAttempt);
+}
+@Service
+class MultipleServiceImpl implements MultipleService{
+	@Autowired
+	RandomGenService t;
+	
+	public Multiple getMultiple() {
+		int a = t.getGenRandom();
+		int b = t.getGenRandom();
+		return new Multiple(a, b);
+	}
+	@Override
+	public boolean checkAttempt(MultiplicationResultAttempt restultAttempt) {
+		return false;
+	}
+}
+
 @RequiredArgsConstructor
-class Tiger {
-	final String name;
-	final int age;
-	Tiger(){
-		this("",0);
+@ToString
+@Getter
+class User{
+	final String alias;
+	User(){
+		// this(null);
+		alias = "익명";
 	}
 }
 @SpringBootTest
 class Msa01ApplicationTests{
 	@Test
 	void test01() {
-		Tiger t1 = new Tiger();
-		Tiger t2 = new Tiger("호랑이",10);
-		System.out.println(t2.toString());
+		User user1 = new User("호랑이");
+		User user2 = new User("호랑이");
+		System.out.println(user1.toString());
+		System.out.println(user1.getAlias());
 	}
 }
 
