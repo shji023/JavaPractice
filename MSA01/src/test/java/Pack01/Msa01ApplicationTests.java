333a3c09c7cd6240775fab45ec0a2f1046ba6ad9
jiyoung <shyunji023@gmail.com>
2022-04-20 09:45:30 +0900

    완성

--- MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
+++ MSA01/src/test/java/Pack01/Msa01ApplicationTests.java
@@ -11,7 +11,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 import static org.mockito.BDDMockito.*;
-
+class Multiple {
+	int factorA, factorB;
+	int result;
+	Multiple(int factorA, int factorB){
+		this.factorA = 1;
+		this.factorB = 2;
+		this.result = this.factorA*this.factorB;
+	}
+	void show() {
+		System.out.println("factorA:"+factorA);
+		System.out.println("factorB:"+factorB);
+		System.out.println("result:"+result);
+	}
+}
 interface RandomGenService{
 	int getGenRandom();
 }
@@ -19,6 +32,17 @@ class RandomGenServiceImpl implements RandomGenService{
 	@Override
 	public int getGenRandom() {
 		return new Random().nextInt(10);
+	}
+}
+// 곱셈 문제 생성지
+interface MultipleGenService {
+	Multiple getMultiple(); 
+}
+class MultipleGenServiceImpl implements MultipleGenService{
+	@Override
+	public Multiple getMultiple() {
+		RandomGenServiceImpl r = new RandomGenServiceImpl();
+		return new Multiple(r.getGenRandom(), r.getGenRandom());
 	}
 }
 @SpringBootTest
