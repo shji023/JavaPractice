0603b4326d045eb2b4cb2308f0b3358c3d1fb016
jiyoung <shyunji023@gmail.com>
2022-04-21 15:15:11 +0900

    BackupSpring-기초 스프링 파일 생성

---
+++ BackupSpring/src/main/java/Pack01/BackupSpringApplication.java
@@ -0,0 +1,13 @@
+package Pack01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupSpringApplication.class, args);
+	}
+
+}
---
+++ BackupSpring/src/main/java/Pack01/controller.java
@@ -0,0 +1,21 @@
+package Pack01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class controller {
+	@RequestMapping("/")
+	public String f0() {
+		System.out.println("f0 call");
+		
+		return "index";
+	}
+	@RequestMapping("/t1")
+	public String f1() {
+		System.out.println("f1 call");
+		
+		return "controllerView";
+		// return "redirect:/";
+	}
+}
