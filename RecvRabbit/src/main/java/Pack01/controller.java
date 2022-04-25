04d59f3fb3c772aeef18c831612026ab55029752
jiyoung <shyunji023@gmail.com>
2022-04-25 11:16:37 +0900

    RabbitMQ Send&Recv

---
+++ RecvRabbit/src/main/java/Pack01/controller.java
@@ -0,0 +1,38 @@
+package Pack01;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.amqp.core.ExchangeTypes;
+
+@Controller 
+public class controller {
+	@RequestMapping("/")
+	public String f0() {
+		System.out.println("f0 call");
+		
+		return "index";
+	}
+	@RequestMapping("/t1")
+	public String f1() {
+		System.out.println("f1 call");
+		
+		return "controllerView";
+		// return "redirect:/";
+	}
+}
+
+@Component
+class RecvModule{
+	@RabbitListener(
+		bindings = @QueueBinding(
+				exchange = @Exchange(name="time",type=ExchangeTypes.TOPIC),
+				value=@Queue(name="banana1"), // 받는키
+				key="routingKey01")			  // 주는키
+		)
+	public void receiver(String msg) {
+		System.out.println(msg);
+	}
+			
+}
---
+++ SendRabbit/src/main/java/Pack01/controller.java
@@ -0,0 +1,38 @@
+package Pack01;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class controller {
+	@RequestMapping("/")
+	public String f0() {
+		System.out.println("f0 call");
+		return "index";
+	}
+	@RequestMapping("/t1")
+	public String f1() {
+		System.out.println("f1 call");
+		
+		return "controllerView";
+		// return "redirect:/";
+	}
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+	static int count = 0;
+	@RequestMapping("/t2")
+	public String f2() {
+		System.out.println("f2 call");
+		System.out.println(rabbitTemplate);
+		String str = "호랑이" +  count++;
+		rabbitTemplate.convertAndSend(
+				"exchange01",
+				"routingKey01", // 주는놈의 키
+				str
+				);
+		// return "controllerView";
+		return "redirect:/";
+	}
+}
