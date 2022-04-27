77bd64b142565a6bb74c0212928d68888aa96841
jiyoung <shyunji023@gmail.com>
2022-04-27 11:35:49 +0900

    unack 테스트

--- MSA04/src/main/java/Pack01/controller.java
+++ MSA04/src/main/java/Pack01/controller.java
@@ -1,10 +1,14 @@
 package Pack01;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+// import확인 필요 AMQP들어가있으면 안됨
+import com.rabbitmq.client.Channel;
 
 @Controller 
 public class controller {
@@ -26,8 +30,19 @@ public class controller {
 @RabbitListener(queues="helloQueue")
 class Tut1Receiver{
 	@RabbitHandler
-	public void receive(String in) {
-		try {Thread.sleep(2000);}catch(Exception e) {}
+	// public void receive(String in) {
+	public void receive(String in, 
+			Channel channel, 
+			Message msg) {
+		System.out.println(channel.toString());
+		try {Thread.sleep(5000);}catch(Exception e) {}
 		System.out.println(in);
+		long tag = msg.getMessageProperties().getDeliveryTag();
+		// 작업이 완료되었음을 Rabbit에게 알림.
+		try {
+			channel.basicAck(tag, false);	
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
